56de7c6e6603173f3e1dc6b75b9fc687872f3c73
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-03-08 20:00:43 -0700

    11.6 saving point. Upload of updated html and css files

--- static/C/JavaScript/app.js
+++ static/C/JavaScript/app.js
@@ -1,3 +1,9 @@
+// import data from data.js
+const tableData = data;
+
+// reference the HTML table(output) using d3 library
+var tbody = d3.select('tbody');
+
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
